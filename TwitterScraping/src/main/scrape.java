9bf403106698d20b7e109223a9fe6389cc66653c
Srobison12 <36313468+Srobison12@users.noreply.github.com>
2021-03-16 15:48:43 -0500

    Adding files needed

---
+++ TwitterScraping/src/main/scrape.java
@@ -0,0 +1,13 @@
+package main;
+
+
+//what java libraries connect with twitter
+
+public class scrape {
+	public static void main(String args[]) {
+		//connect with twitter
+		//only 1 request per second
+		//read any tweet and print the tweet
+		
+	}
+}
